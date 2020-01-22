1bc6a9e3969e597ede863ad13ccb371f5a668653
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-22 23:13:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,6 +16,7 @@ def speak_to_grandma()
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "WHAT DID YOU EAT TODAY?"
     "NO, NOT SINCE 1938!"
+  elsif phrase == "WHAT?"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
