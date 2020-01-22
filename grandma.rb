42c8e57e05350a1109017472cd7620bdb8c806f1
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-22 23:14:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,7 @@ def speak_to_grandma()
   elsif phrase == "WHAT DID YOU EAT TODAY?"
     "NO, NOT SINCE 1938!"
   elsif phrase == "WHAT?"
+    "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
